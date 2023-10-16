50510e2f19727a93bfdd43bede3a88047798b989
smku255mku255C2S21254 <147838305+smku255mku255C2S21254@users.noreply.github.com>
2023-10-16 15:24:45 +0530

    Add files via upload

---
+++ main (17).py
@@ -0,0 +1,39 @@
+Programiz
+
+Search...
+Programiz PRO
+
+
+Python Program to Check Leap Year
+To understand this example, you should have the knowledge of the following Python programming topics:
+
+Python Operators
+Python if...else Statement
+A leap year is exactly divisible by 4 except for century years (years ending with 00). The century year is a leap year only if it is perfectly divisible by 400. For example,
+
+2017 is not a leap year
+1900 is a not leap year
+2012 is a leap year
+2000 is a leap year
+Source Code
+# Python program to check if year is a leap year or not
+
+year = 2000
+
+# To get year (integer input) from the user
+# year = int(input("Enter a year: "))
+
+# divided by 100 means century year (ending with 00)
+# century year divided by 400 is leap year
+if (year % 400 == 0) and (year % 100 == 0):
+    print("{0} is a leap year".format(year))
+
+# not divided by 100 means not a century year
+# year divided by 4 is a leap year
+elif (year % 4 ==0) and (year % 100 != 0):
+    print("{0} is a leap year".format(year))
+
+# if not divided by both 400 (century year) and 4 (not century year)
+# year is not leap year
+else:
+    print("{0} is not a leap year".format(year))
